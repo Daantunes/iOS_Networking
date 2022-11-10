423a08408ce07fe70d26a3d0b8399c3255dc01f0
Débora <d.antunes@kigroup.de>
2022-11-10 10:01:20 +0000

    feat(language-requests): add language requests

--- NetworkingExampleApp/App/UpdateRingView.swift
+++ NetworkingExampleApp/App/UpdateRingView.swift
@@ -7,6 +7,7 @@ struct UpdateRingView: View {
 
   let ring: Ring
   @State var ringName: String
+  @State var languages: [Language] = []
 
   init(ring: Ring) {
     self.ring = ring
@@ -15,17 +16,62 @@ struct UpdateRingView: View {
 
   var body: some View {
     VStack(alignment: .leading) {
-      Text("Ring Name")
+      Text("Ring Name:")
         .font(.callout)
         .bold()
       TextField("Enter ring name...", text: $ringName)
         .textFieldStyle(.roundedBorder)
+      Spacer(minLength: 50)
+      if !languages.isEmpty {
+        Text("Languages:")
+          .font(.callout)
+          .bold()
+      }
+      List {
+        ForEach(languages) { language in
+          Text(language.name)
+        }
+        .onDelete() { indexSet in
+          for index in indexSet {
+            deleteLanguage(id: languages[index].id)
+            languages.remove(at: index)
+          }
+        }
+      }
+      .listStyle(.inset)
+      NavigationLink("Add Language") { LanguageDetailView(ring: ring) }
       Spacer()
     }
     .padding()
+    .onAppear {
+      getRingLanguages()
+    }
     .navigationBarItems(trailing: Button(action: {}) {
       doneButton
     })
+  }
+
+  private func deleteLanguage(id: UUID) {
+    RequestManager().sendRequest(router: .deleteLanguage(id: id)) { completion in
+      switch completion {
+        case .success:
+          getRingLanguages()
+        case .failure(let failure):
+          print(failure)
+      }
+    }
+  }
+
+  private func getRingLanguages() {
+    RequestManager()
+      .sendRequest(router: .getRingLanguages(id: ring.id), responseModel: [Language].self) { completion in
+        switch completion {
+          case .success(let languages):
+            self.languages = languages
+          case .failure(let error):
+            print(error)
+        }
+      }
   }
 
   private var doneButton: some View {
@@ -49,6 +95,6 @@ struct UpdateRingView: View {
 
 struct UpdateRingView_Previews: PreviewProvider {
   static var previews: some View {
-    UpdateRingView(ring: Ring(id: UUID(), name: "Dummy", languages: []))
+    UpdateRingView(ring: Ring(id: UUID(uuidString: "DD7DCC97-C61D-4AB9-B80E-7C87CFE66831")!, name: "Dummy", languages: []))
   }
 }
--- NetworkingExampleApp/Model/Language.swift
+++ NetworkingExampleApp/Model/Language.swift
@@ -3,6 +3,5 @@ import Foundation
 struct Language: Codable, Identifiable {
   let id: UUID
   let name: String
-  let ringId: UUID
 }
 
